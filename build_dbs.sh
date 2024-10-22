4183d58167f5a0c325e5223030557d64692c9d71
maxxsyntax <maxxsyntax@localhost>
2024-10-22 16:48:24 -0600

    fixing errors in db

--- build_dbs.sh
+++ build_dbs.sh
@@ -3,7 +3,7 @@
 mysql probeprint "create table ssid(ssid_hex varchar(200), wlan_sa varchar(17), time varchar(22) primary key, rssi varchar(12), freq integer, seq integer, vht varchar(20), is_processed integer default 0, vendor text default null, tag text default null);"
 
 
-mysql probeprint "create table ssid_intel(ssid_hex varchar(255) primary key, score integer, location varchar(64), category varchar(32), is_name varchar(20),is_airport varchar(255), is_common integer default null, is_oneloc integer, is_vht integer);"
+mysql probeprint "create table ssid_intel(ssid_hex varchar(255) primary key, location varchar(64), category varchar(32), is_name varchar(20),is_airport varchar(255), is_common integer default null, is_oneloc integer);"
 
 mysql probeprint "create table bursts(ssids text, time varchar(22) primary key, burst_size integer, burst_duration varchar(22) default 0, related_burst integer default 0, is_uniq integer default null, bmethod varchar(20) default null);
 
